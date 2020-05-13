33d511315a0961f30b6c056bc394574eb0597a29
Larry Kagan <larry@superiocity.com>
2020-05-13 18:38:24 -0400

    Accept a body argument

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Note content',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ', argv.title, "\nBody: ", argv.body);
     }
 });
 
@@ -37,6 +49,4 @@ yargs.command({
     }
 });
 
-// add, remove, read, list
-console.log(yargs.argv);
-
+yargs.parse();
